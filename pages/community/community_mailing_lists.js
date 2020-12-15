52988e576fd842185909ba75aad8008b2ccbb472
Kanishka Azimi <kazimi@solvuu.com>
2020-12-15 15:04:47 -0500

    All content copied, some styling, needs italics and more accurate margin and text sizes

--- pages/community/community_mailing_lists.js
+++ pages/community/community_mailing_lists.js
@@ -84,7 +84,85 @@ function $$default(param) {
                 }, "The ", React.createElement(Community_mailing_lists$AEXT, {
                       children: "OCaml Weekly News",
                       href: "http://alan.petitepomme.net/cwn/"
-                    }), " also provides a curated summary of camll-list discussions."));
+                    }), " also provides a curated summary of camll-list discussions."), React.createElement(Community_mailing_lists$H3, {
+                  children: "OCaml Jobs and Internships"
+                }), React.createElement(Community_mailing_lists$P, {
+                  children: "ocaml-jobs AT inria.fr This list is for exchanges \n    between people looking for a job or an internship requiring skills \n    in OCaml and people, corporations, universities, ..., offering such jobs \n    or internships."
+                }), React.createElement(Community_mailing_lists$P, {
+                  children: React.createElement(Community_mailing_lists$AEXT, {
+                        children: "(Un)subscribe",
+                        href: "https://sympa.inria.fr/sympa/info/ocaml-jobs"
+                      })
+                }), React.createElement(Community_mailing_lists$H3, {
+                  children: "OCaml Announcements"
+                }), React.createElement(Community_mailing_lists$P, {
+                  children: "caml-announce AT inria.fr This is a low-traffic, moderated list \n    for announcements of OCaml releases and new OCaml-related software, libraries, \n    documents, etc."
+                }), React.createElement(Community_mailing_lists$P, {
+                  children: React.createElement(Community_mailing_lists$AEXT, {
+                        children: "(Un)subscribe",
+                        href: "https://sympa.inria.fr/sympa/subscribe/caml-announce"
+                      })
+                }), React.createElement(Community_mailing_lists$H2, {
+                  children: "Discussion Groups"
+                }), React.createElement(Community_mailing_lists$H3, {
+                  children: "IRC Channel - English"
+                }), React.createElement(Community_mailing_lists$P, {
+                  children: "irc.freenode.net #ocaml This is a real-time communication channel, where \n    you can ask for help. There are about a hundred users hanging around; don't ask \n    if you can ask, just ask, and be patient: not everyone is in the same timezone. \n    The IRC Channel can be accessed through a web interface or any regular IRC client."
+                }), React.createElement(Community_mailing_lists$P, {
+                  children: null
+                }, "Public channel logs are available at ", React.createElement(Community_mailing_lists$AEXT, {
+                      children: "http://irclog.whitequark.org/ocaml/",
+                      href: "http://irclog.whitequark.org/ocaml/"
+                    })), React.createElement(Community_mailing_lists$P, {
+                  children: null
+                }, "If you wish to use a web-based IRC client, you can use Freenode's webchat ", React.createElement(Community_mailing_lists$AEXT, {
+                      children: "https://webchat.freenode.net/",
+                      href: "https://webchat.freenode.net/"
+                    }), "."), React.createElement(Community_mailing_lists$H3, {
+                  children: "Discord Server"
+                }), React.createElement(Community_mailing_lists$P, {
+                  children: null
+                }, React.createElement(Community_mailing_lists$AEXT, {
+                      children: "discord link",
+                      href: "https://discord.gg/cCYQbqN"
+                    }), ": As a more recent addition to the OCaml community, the OCaml discord \n    server benefits from the proximity of the ReasonML community's discord server, as well \n    as discord's ability to have multiple channels. One of the channels is called #IRC, \n    and automatically connects to the main IRC channel."), React.createElement(Community_mailing_lists$H3, {
+                  children: "IRC Channel - French"
+                }), React.createElement(Community_mailing_lists$P, {
+                  children: "irc.freenode.net #ocaml-fr As above, but for French speakers."
+                }), React.createElement(Community_mailing_lists$H3, {
+                  children: "About ML"
+                }), React.createElement(Community_mailing_lists$P, {
+                  children: "comp.lang.ml This is a moderated Usenet newsgroup about all variants of ML. \n    Discussions generally concern Standard ML implementations (such as SML-NJ), but some threads \n    concern the OCaml branch."
+                }), React.createElement(Community_mailing_lists$P, {
+                  children: null
+                }, React.createElement(Community_mailing_lists$AEXT, {
+                      children: "Archives at Google Groups",
+                      href: "http://groups.google.com/groups?group=comp.lang.ml"
+                    }), " | ", React.createElement(Community_mailing_lists$AEXT, {
+                      children: "FAQ",
+                      href: "http://www.faqs.org/faqs/meta-lang-faq/"
+                    })), React.createElement(Community_mailing_lists$H3, {
+                  children: "About Functional Languages"
+                }), React.createElement(Community_mailing_lists$P, {
+                  children: "comp.lang.functional This is an unmoderated usenet newsgroup for the discussion \n    of all aspects of functional programming languages, including their design, application, \n    theoretical foundation, and implementation. Discussions concern all families of functional \n    programming languages including non-strict ones (e.g. Haskell) and strict ones (e.g. Scheme, \n    SML or OCaml)."
+                }), React.createElement(Community_mailing_lists$P, {
+                  children: null
+                }, React.createElement(Community_mailing_lists$AEXT, {
+                      children: "Archives at Google Groups",
+                      href: "http://groups.google.com/groups?group=comp.lang.functional"
+                    }), " | ", React.createElement(Community_mailing_lists$AEXT, {
+                      children: "FAQ",
+                      href: "http://www.cs.nott.ac.uk/~gmh/faq.html"
+                    })), React.createElement(Community_mailing_lists$H2, {
+                  children: "Other"
+                }), React.createElement(Community_mailing_lists$H3, {
+                  children: "Bug Tracker"
+                }), React.createElement(Community_mailing_lists$P, {
+                  children: null
+                }, "Use ", React.createElement(Community_mailing_lists$AEXT, {
+                      children: "Github issues",
+                      href: "https://github.com/ocaml/ocaml/issues"
+                    }), " to request features or report bugs."));
 }
 
 export {
--- pages/community/community_mailing_lists.re
+++ pages/community/community_mailing_lists.re
@@ -72,4 +72,69 @@ let default = () =>
     <AEXT href="http://alan.petitepomme.net/cwn/">{React.string("OCaml Weekly News")}</AEXT>
     {React.string(" also provides a curated summary of camll-list discussions.")}
     </P>
+    <H3>{React.string("OCaml Jobs and Internships")}</H3>
+    <P>{React.string("ocaml-jobs AT inria.fr This list is for exchanges 
+    between people looking for a job or an internship requiring skills 
+    in OCaml and people, corporations, universities, ..., offering such jobs 
+    or internships.")}
+    </P>
+    <P>
+    <AEXT href="https://sympa.inria.fr/sympa/info/ocaml-jobs">{React.string("(Un)subscribe")}</AEXT>
+    </P>
+    <H3>{React.string("OCaml Announcements")}</H3>
+    <P>{React.string("caml-announce AT inria.fr This is a low-traffic, moderated list 
+    for announcements of OCaml releases and new OCaml-related software, libraries, 
+    documents, etc.")}
+    </P>
+    <P>
+    <AEXT href="https://sympa.inria.fr/sympa/subscribe/caml-announce">{React.string("(Un)subscribe")}</AEXT>
+    </P>
+    <H2>{React.string("Discussion Groups")}</H2>
+    <H3>{React.string("IRC Channel - English")}</H3>
+    <P>{React.string("irc.freenode.net #ocaml This is a real-time communication channel, where 
+    you can ask for help. There are about a hundred users hanging around; don't ask 
+    if you can ask, just ask, and be patient: not everyone is in the same timezone. 
+    The IRC Channel can be accessed through a web interface or any regular IRC client.")} 
+    </P>
+    <P>{React.string("Public channel logs are available at ")}
+    <AEXT href="http://irclog.whitequark.org/ocaml/">{React.string("http://irclog.whitequark.org/ocaml/")}</AEXT>     
+    </P>
+    <P>{React.string("If you wish to use a web-based IRC client, you can use Freenode's webchat ")}
+    <AEXT href="https://webchat.freenode.net/">{React.string("https://webchat.freenode.net/")}</AEXT>
+    {React.string(".")}
+    </P>
+    <H3>{React.string("Discord Server")}</H3>
+    <P>
+    <AEXT href="https://discord.gg/cCYQbqN">{React.string("discord link")}</AEXT>
+    {React.string(": As a more recent addition to the OCaml community, the OCaml discord 
+    server benefits from the proximity of the ReasonML community's discord server, as well 
+    as discord's ability to have multiple channels. One of the channels is called #IRC, 
+    and automatically connects to the main IRC channel.")} 
+    </P>
+    <H3>{React.string("IRC Channel - French")}</H3>
+    <P>{React.string("irc.freenode.net #ocaml-fr As above, but for French speakers.")}</P>
+    <H3>{React.string("About ML")}</H3>
+    <P>{React.string("comp.lang.ml This is a moderated Usenet newsgroup about all variants of ML. 
+    Discussions generally concern Standard ML implementations (such as SML-NJ), but some threads 
+    concern the OCaml branch.")}</P>
+    <P><AEXT href="http://groups.google.com/groups?group=comp.lang.ml">{React.string("Archives at Google Groups")}</AEXT>
+    {React.string(" | ")}
+    <AEXT href="http://www.faqs.org/faqs/meta-lang-faq/">{React.string("FAQ")}</AEXT>
+    </P> 
+    <H3>{React.string("About Functional Languages")}</H3>
+    <P>{React.string("comp.lang.functional This is an unmoderated usenet newsgroup for the discussion 
+    of all aspects of functional programming languages, including their design, application, 
+    theoretical foundation, and implementation. Discussions concern all families of functional 
+    programming languages including non-strict ones (e.g. Haskell) and strict ones (e.g. Scheme, 
+    SML or OCaml).")}</P>
+    <P><AEXT href="http://groups.google.com/groups?group=comp.lang.functional">{React.string("Archives at Google Groups")}</AEXT>
+    {React.string(" | ")}
+    <AEXT href="http://www.cs.nott.ac.uk/~gmh/faq.html">{React.string("FAQ")}</AEXT>
+    </P> 
+    <H2>{React.string("Other")}</H2>
+    <H3>{React.string("Bug Tracker")}</H3>
+    <P>{React.string("Use ")}
+    <AEXT href="https://github.com/ocaml/ocaml/issues">{React.string("Github issues")}</AEXT>
+    {React.string(" to request features or report bugs.")}
+    </P>
   </>;
