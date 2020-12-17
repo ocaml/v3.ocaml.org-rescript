b8268eec61abb1125fe19e3f531c6971933e3902
Kanishka Azimi <kazimi@solvuu.com>
2020-12-16 20:29:11 -0500

    Essential content copied, starting to style

--- pages/iasseminar.js
+++ pages/iasseminar.js
@@ -2,17 +2,36 @@
 
 import * as React from "react";
 
-function Iasseminar$H1(Props) {
-  var children = Props.children;
-  return React.createElement("h1", {
-              className: "font-sans text-4xl font-bold leading-snug mb-2"
-            }, children);
-}
-
 function $$default(param) {
-  return React.createElement(React.Fragment, undefined, React.createElement(Iasseminar$H1, {
-                  children: "OCaml Users and Developers Workshop"
-                }));
+  return React.createElement(React.Fragment, undefined, React.createElement("h1", {
+                  className: "ml-10 text-4xl font-bold leading-snug mb-2"
+                }, "Seminars"), React.createElement("p", {
+                  className: "mb-3 ml-20"
+                }, "The Theoretical Computer Science and Discrete Mathematics \n    Seminars will take place every Monday at 11:15 a.m. - 12:15 p.m. and every \n    Tuesday at 10:30 a.m. - 12:30 p.m. at the Institute for Advanced Study. \n    The lectures will be held in S-101, the seminar room in Simonyi Hall, \n    unless stated otherwise."), React.createElement("p", {
+                  className: "mb-3 ml-20"
+                }, "If you are interested in attending future seminars and are \n    not already on our mailing list from previous years, please send an e-mail \n    to Andrea Lass and ask to be added."), React.createElement("h2", {
+                  className: "text-2xl font-bold leading-normal mb-2"
+                }, "Upcoming Seminar Titles Include:"), React.createElement("div", {
+                  className: "flex bg-gray-200"
+                }, React.createElement("div", {
+                      className: "flex-1 text-center bg-gray-400 m-2"
+                    }, React.createElement("p", {
+                          className: "text-xs"
+                        }, "JAN"), React.createElement("p", {
+                          className: "text-2xl font-bold"
+                        }, "25"), React.createElement("p", {
+                          className: "text-xs"
+                        }, "2021")), React.createElement("div", {
+                      className: "flex-1 text-center bg-gray-400 m-2"
+                    }, "IAS GRAPHIC"), React.createElement("div", {
+                      className: "flex-1 bg-gray-400 m-2"
+                    }, React.createElement("p", {
+                          className: "font-bold"
+                        }, "Computer Sciences/Discrete Mathematics Seminar I"), React.createElement("p", undefined, "TBA"), React.createElement("p", {
+                          className: "italic"
+                        }, "Andrew Drucker"), React.createElement("p", {
+                          className: "italic"
+                        }, "11:15am"))), React.createElement("div", undefined, React.createElement("div", undefined, "JAN 26 2021"), React.createElement("div", undefined, "IAS GRAPHIC"), React.createElement("div", undefined, React.createElement("p", undefined, "Computer Sciences/Discrete Mathematics Seminar II"), React.createElement("p", undefined, "TBA"), React.createElement("p", undefined, "Cynthia Vinzant"), React.createElement("p", undefined, "10:30am | Simonyi Hall 101 and Remote Access - see Zoom link below"))), React.createElement("button", undefined, "VIEW MORE"));
 }
 
 export {
--- pages/iasseminar.res
+++ pages/iasseminar.res
@@ -1,39 +1,59 @@
 module Link = Next.Link;
 
-module H1 = {
-  // TODO: use mb-1.5 and tailwind 2
-  @react.component
-  let make = (~children) => 
-    <h1 className="font-sans text-4xl font-bold leading-snug mb-2"> children </h1>;
-};
 module H2 = {
   @react.component
   let make = (~children) => 
     <h2 className="font-sans text-2xl font-bold leading-normal mb-2"> children </h2>;
 };
-module UL = {
-  @react.component
-  let make = (~children) => 
-    <ul className="mb-6 ml-6 -mt-3 list-disc"> children </ul>;
-};
-module LI = {
-  @react.component
-  let make = (~children) => 
-    <li className="mb-3"> children </li>;
-};
-// TODO: I haven't been able to implement "A" without include "Link" yet. Troubleshoot why
-module LINK = {
-  @react.component
-  let make = (~href, ~children) => 
-    <Link href={href}><a className="text-ocamlorange hover:underline"> children </a></Link>;
-};
-module AEXT = {
-  @react.component
-  let make = (~children, ~href) => 
-    <a href={href} className="text-ocamlorange hover:underline" target="_blank" > children </a>;
-};
 
-let default = () =>
+let default = () => {
+  let s = React.string
   <>
-    <H1>{React.string(`OCaml Users and Developers Workshop`)}</H1>
-  </>;
+    <h1 className="ml-10 text-4xl font-bold leading-snug mb-2">{s(`Seminars`)}</h1>
+    <p className="mb-3 ml-20">{s(`The Theoretical Computer Science and Discrete Mathematics 
+    Seminars will take place every Monday at 11:15 a.m. - 12:15 p.m. and every 
+    Tuesday at 10:30 a.m. - 12:30 p.m. at the Institute for Advanced Study. 
+    The lectures will be held in S-101, the seminar room in Simonyi Hall, 
+    unless stated otherwise.`)}
+    </p>
+    <p className="mb-3 ml-20">{s(`If you are interested in attending future seminars and are 
+    not already on our mailing list from previous years, please send an e-mail 
+    to Andrea Lass and ask to be added.`)}
+    </p>
+
+    <h2 className="text-2xl font-bold leading-normal mb-2">
+      {s(`Upcoming Seminar Titles Include:`)}
+    </h2>
+
+    <div className="flex bg-gray-200">
+        <div className="flex-1 text-center bg-gray-400 m-2">
+            <p className="text-xs">{s(`JAN`)}</p>
+            <p className="text-2xl font-bold">{s(`25`)}</p>
+            <p className="text-xs">{s(`2021`)}</p>
+        </div>
+        <div className="flex-1 text-center bg-gray-400 m-2">{
+            s(`IAS GRAPHIC`)}
+        </div>
+        <div className="flex-1 bg-gray-400 m-2">
+          <p className="font-bold">{s(`Computer Sciences/Discrete Mathematics Seminar I`)}</p>
+          <p>{s(`TBA`)}</p>
+          <p className="italic">{s(`Andrew Drucker`)}</p>
+          <p className="italic">{s(`11:15am`)}</p>
+        </div>
+    </div>
+
+    <div>
+        <div>{s(`JAN 26 2021`)}</div>
+        <div>{s(`IAS GRAPHIC`)}</div>
+        <div>
+          <p>{s(`Computer Sciences/Discrete Mathematics Seminar II`)}</p>
+          <p>{s(`TBA`)}</p>
+          <p>{s(`Cynthia Vinzant`)}</p>
+          <p>{s(`10:30am | Simonyi Hall 101 and Remote Access - see Zoom link below`)}</p>
+        </div>
+    </div>
+
+    <button>{s(`VIEW MORE`)}</button>
+
+  </>
+}
