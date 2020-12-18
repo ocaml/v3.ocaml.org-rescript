34406880212779f2676683b1df4675843ef05f0e
Kanishka Azimi <kazimi@solvuu.com>
2020-12-17 23:26:57 -0500

    Slightly more accurate, much more to go

--- pages/quantacs.js
+++ pages/quantacs.js
@@ -9,20 +9,24 @@ function $$default(param) {
                       className: "text-center pt-20 mb-4 text-xs"
                     }, "WHAT\'S UP IN"), React.createElement("h1", {
                       className: "text-center text-5xl font-bold leading-snug mb-16"
-                    }, "Computer Science"), React.createElement("div", undefined, React.createElement("div", {
-                          className: "flex bg-red-200"
-                        }, React.createElement("div", undefined, React.createElement("img", {
-                                  className: "w-64 bg-orange-100",
-                                  src: "static/Jelani-Nelson_520x292-520x292.jpeg"
-                                })), React.createElement("div", undefined, React.createElement("p", {
-                                  className: "bg-green-200"
-                                }, "Q & A"), React.createElement("p", {
-                                  className: "bg-white"
-                                }, "By ALLISON WHITTEN | DECEMBER 7, 2020 | ch-ic | tag-ic"), React.createElement("p", {
-                                  className: "bg-blue-100"
-                                }, "Jelani Nelson designs clever algorithms that only \n              have to remember slivers of massive data sets. He also\n              teaches kids in Ethiopia how to code."))), React.createElement("div", {
-                          className: "bg-yellow-100"
-                        }, React.createElement("h2", undefined, "Topics"), React.createElement("div", undefined, "Search"), React.createElement("h3", undefined, "View All Topics"), React.createElement("h2", undefined, "Follow Quanta")))));
+                    }, "Computer Science"), React.createElement("div", {
+                      className: "p-16 bg-white"
+                    }, React.createElement("div", {
+                          className: "flex bg-black space-x-20"
+                        }, React.createElement("div", {
+                              className: "flex bg-red-200 space-x-8"
+                            }, React.createElement("div", undefined, React.createElement("img", {
+                                      className: "w-64 bg-orange-100",
+                                      src: "static/Jelani-Nelson_520x292-520x292.jpeg"
+                                    })), React.createElement("div", undefined, React.createElement("p", {
+                                      className: "bg-green-200"
+                                    }, "Q & A"), React.createElement("p", {
+                                      className: "bg-white"
+                                    }, "By ALLISON WHITTEN | DECEMBER 7, 2020 | ch-ic | tag-ic"), React.createElement("p", {
+                                      className: "bg-blue-100"
+                                    }, "Jelani Nelson designs clever algorithms that only \n                have to remember slivers of massive data sets. He also\n                teaches kids in Ethiopia how to code."))), React.createElement("div", {
+                              className: "bg-yellow-100"
+                            }, React.createElement("h2", undefined, "Topics"), React.createElement("div", undefined, "Search"), React.createElement("h3", undefined, "View All Topics"), React.createElement("h2", undefined, "Follow Quanta"))))));
 }
 
 export {
--- pages/quantacs.res
+++ pages/quantacs.res
@@ -4,28 +4,31 @@ let default = () => {
   let s = React.string
   <>
 <div className="bg-gray-300 min-w-full">
+
 <p className="text-center pt-20 mb-4 text-xs">{s(`WHAT'S UP IN`)}</p>
 <h1 className="text-center text-5xl font-bold leading-snug mb-16">{s(`Computer Science`)}</h1>
 
-<div>
-  <div className="flex bg-red-200">
-    <div>
-      <img className="w-64 bg-orange-100" src="static/Jelani-Nelson_520x292-520x292.jpeg"/>
+<div className="p-16 bg-white">
+  <div className="flex bg-black space-x-20">
+    <div className="flex bg-red-200 space-x-8">
+      <div>
+        <img className="w-64 bg-orange-100" src="static/Jelani-Nelson_520x292-520x292.jpeg"/>
+      </div>
+      <div>
+        <p className="bg-green-200">{s(`Q & A`)}</p>
+        <p className="bg-white">{s(`By ALLISON WHITTEN | DECEMBER 7, 2020 | ch-ic | tag-ic`)}</p>
+        <p className="bg-blue-100">{s(`Jelani Nelson designs clever algorithms that only 
+                have to remember slivers of massive data sets. He also
+                teaches kids in Ethiopia how to code.`)}</p>
+      </div>
     </div>
-    <div>
-      <p className="bg-green-200">{s(`Q & A`)}</p>
-      <p className="bg-white">{s(`By ALLISON WHITTEN | DECEMBER 7, 2020 | ch-ic | tag-ic`)}</p>
-      <p className="bg-blue-100">{s(`Jelani Nelson designs clever algorithms that only 
-              have to remember slivers of massive data sets. He also
-              teaches kids in Ethiopia how to code.`)}</p>
-    </div>
-  </div>
 
-  <div className="bg-yellow-100">
-    <h2>{s(`Topics`)}</h2>
-    <div>{s(`Search`)}</div>
-    <h3>{s(`View All Topics`)}</h3>
-    <h2>{s(`Follow Quanta`)}</h2>    
+    <div className="bg-yellow-100">
+      <h2>{s(`Topics`)}</h2>
+      <div>{s(`Search`)}</div>
+      <h3>{s(`View All Topics`)}</h3>
+      <h2>{s(`Follow Quanta`)}</h2>    
+    </div>
   </div>
 </div>
 
