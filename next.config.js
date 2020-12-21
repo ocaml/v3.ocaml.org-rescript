a4fe368022b009a28a6c72ee3d688e891bb97bd5
Kanishka Azimi <kazimi@solvuu.com>
2020-12-21 15:51:14 -0500

    mdxjs deps added, site building

--- next.config.js
+++ next.config.js
@@ -4,14 +4,19 @@ const withCSS = require('@zeit/next-css');
 const transpileModules = ["bs-platform"].concat(bsconfig["bs-dependencies"]);
 const withTM = require("next-transpile-modules")(transpileModules);
 
+// TODO: why does rescxript lang define plugin locally
+const withMdx = require("@next/mdx")({
+  extension: /\.mdx?$/,
+});
+
 const config = {
   target: "serverless",
-  pageExtensions: ["jsx", "js"],
+  pageExtensions: ["jsx", "js", "mdx"],
   transpileModules: ["bs-platform"].concat(bsconfig["bs-dependencies"]),
   env: {
     ENV: process.env.NODE_ENV,
   },
 };
 
-module.exports = withTM(withCSS(config));
+module.exports = withMdx(withTM(withCSS(config)));
 
