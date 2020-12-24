2cf6f0e06d869fb6f217056c204c6c23bc803f2d
Kanishka Azimi <kazimi@solvuu.com>
2020-12-24 16:30:24 -0500

    Remove nanocss, make required node version more precise

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,6 @@
-const plugins ={
-  tailwindcss: {},
-  autoprefixer: {}
-};
-
-if(process.env.NODE_ENV === 'production') {
-  plugins.cssnano = {};
-}
-
 module.exports = {
-  plugins: plugins
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
