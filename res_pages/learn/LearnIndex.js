3e9b59e2ca6a75bbce99f04c5d730d47a27a78a1
Kanishka Azimi <kazimi@solvuu.com>
2020-12-28 19:22:50 -0500

    Some layout and content for first two rows of learn landing page

---
+++ res_pages/learn/LearnIndex.js
@@ -0,0 +1,78 @@
+
+
+import * as React from "react";
+
+function $$default(param) {
+  return React.createElement(React.Fragment, undefined, React.createElement("h1", {
+                  className: "text-4xl font-bold py-2"
+                }, "Learn"), React.createElement("div", {
+                  className: "flex space-x-8 py-4"
+                }, React.createElement("img", {
+                      className: "flex-0",
+                      src: "static/learn-large.svg"
+                    }), React.createElement("div", {
+                      className: "flex-auto"
+                    }, "What is OCaml? Ocaml is a general purpose\n        industrial-strength programming language with an emphasis on\n        expressiveness and safety. It is the technology of choice in ", "companies", " where a single mistake can cost millions and speed matters,\n        and there is an ", "active community", " that has developed a ", "rich set of libraries", ". It\'s also a widely used ", "teaching language", ".", "Read more", "."), React.createElement("div", {
+                      className: "flex-auto flex align-center items-center"
+                    }, React.createElement("span", undefined, React.createElement("a", {
+                              className: "whitespace-no-wrap text-center text-white font-bold inline-block px-2 py-1 bg-green-800",
+                              href: ""
+                            }, "Install OCaml")))), React.createElement("div", {
+                  className: "flex space-x-8"
+                }, React.createElement("div", {
+                      className: "flex-1"
+                    }, React.createElement("div", {
+                          className: "border-b border-gray-500"
+                        }, React.createElement("h2", {
+                              className: "text-2xl font-semibold"
+                            }, "Code Examples"))), React.createElement("div", {
+                      className: "flex-1"
+                    }, React.createElement("div", {
+                          className: "border-b border-gray-500"
+                        }, React.createElement("h2", {
+                              className: "text-2xl font-semibold"
+                            }, "Tutorials & FAQ")), React.createElement("ul", {
+                          className: "px-4 list-disc list-inside space-y-3"
+                        }, React.createElement("li", undefined, "Up and Running"), React.createElement("li", undefined, "Basics"), React.createElement("li", undefined, "Structure of OCaml Programs"))), React.createElement("div", {
+                      className: "flex-1"
+                    }, React.createElement("div", {
+                          className: "border-b border-gray-500"
+                        }, React.createElement("h2", {
+                              className: "text-2xl font-semibold"
+                            }, "Books")), React.createElement("img", {
+                          className: "float-left py-3 w-2/5",
+                          src: "static/real-world-ocaml.jpeg"
+                        }), React.createElement("img", {
+                          className: "float-left py-3 w-2/5",
+                          src: "static/OCaml_from_beginning.png"
+                        }), React.createElement("span", {
+                          className: "clearfix"
+                        }), React.createElement("p", undefined, "There are a number of excellent books, with two new \n            titles published in recent years."))), React.createElement("div", {
+                  className: "flex space-x-8"
+                }, React.createElement("div", {
+                      className: "flex-1"
+                    }, React.createElement("div", {
+                          className: "border-b border-gray-500"
+                        }, React.createElement("h2", {
+                              className: "text-2xl font-semibold"
+                            }, "Online Courses, Slides & Videos"))), React.createElement("div", {
+                      className: "flex-1"
+                    }, React.createElement("div", {
+                          className: "border-b border-gray-500"
+                        }, React.createElement("h2", {
+                              className: "text-2xl font-semibold"
+                            }, "Industrial Users"))), React.createElement("div", {
+                      className: "flex-1"
+                    }, React.createElement("div", {
+                          className: "border-b border-gray-500"
+                        }, React.createElement("h2", {
+                              className: "text-2xl font-semibold"
+                            }, "Success Stories")))));
+}
+
+export {
+  $$default ,
+  $$default as default,
+  
+}
+/* react Not a pure module */
--- res_pages/learn/LearnIndex.res
+++ res_pages/learn/LearnIndex.res
@@ -1,1 +1,78 @@
+let default = () => {
+  let s = React.string
+  <>
+    <h1 className="text-4xl font-bold py-2">{s(`Learn`)}</h1>
+    <div className="flex space-x-8 py-4">
+        <img src="static/learn-large.svg" className="flex-0" />
+        <div className="flex-auto">{s(`What is OCaml? Ocaml is a general purpose
+        industrial-strength programming language with an emphasis on
+        expressiveness and safety. It is the technology of choice in `)}
+        {s(`companies`)}
+        {s(` where a single mistake can cost millions and speed matters,
+        and there is an `)}
+        {s(`active community`)}
+        {s(` that has developed a `)}
+        {s(`rich set of libraries`)}
+        {s(`. It's also a widely used `)}
+        {s(`teaching language`)}
+        {s(`.`)}
+        {s(`Read more`)}
+        {s(`.`)}
+        </div>
+        <div className="flex-auto flex align-center items-center">
+            <span>
+            <a className="whitespace-no-wrap text-center text-white font-bold inline-block px-2 py-1 bg-green-800" href="">
+            {s(`Install OCaml`)}
+            </a>
+            </span>
+        </div>
+    </div>
 
+    <div className="flex space-x-8">
+        <div className="flex-1">
+            <div className="border-b border-gray-500">
+                <h2 className="text-2xl font-semibold">{s(`Code Examples`)}</h2>
+            </div>
+        </div>
+        <div className="flex-1">
+            <div className="border-b border-gray-500">
+                <h2 className="text-2xl font-semibold">{s(`Tutorials & FAQ`)}</h2>
+            </div>
+            <ul className="px-4 list-disc list-inside space-y-3">
+                <li>{s("Up and Running")}</li>
+                <li>{s("Basics")}</li>
+                <li>{s("Structure of OCaml Programs")}</li>
+            </ul>
+        </div>
+        <div className="flex-1">
+            <div className="border-b border-gray-500">
+                <h2 className="text-2xl font-semibold">{s(`Books`)}</h2>
+            </div>
+            <img src="static/real-world-ocaml.jpeg" className="float-left py-3 w-2/5"/>
+            <img src="static/OCaml_from_beginning.png" className="float-left py-3 w-2/5"/>
+            <span className="clearfix" />
+            <p>{s(`There are a number of excellent books, with two new 
+            titles published in recent years.`)}</p>
+        </div>
+    </div>
+
+    <div className="flex space-x-8">
+        <div className="flex-1">
+            <div className="border-b border-gray-500">
+                <h2 className="text-2xl font-semibold">{s(`Online Courses, Slides & Videos`)}</h2>
+            </div>
+        </div>
+        <div className="flex-1">
+            <div className="border-b border-gray-500">
+                <h2 className="text-2xl font-semibold">{s(`Industrial Users`)}</h2>
+            </div>
+        </div>
+        <div className="flex-1">
+            <div className="border-b border-gray-500">
+                <h2 className="text-2xl font-semibold">{s(`Success Stories`)}</h2>
+            </div>
+        </div>
+    </div>
+
+  </>
+}
