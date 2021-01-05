8144fc0001f4183030be16d5b8a2a0f2ed87c1d5
Kanishka Azimi <kazimi@solvuu.com>
2021-01-05 01:07:53 -0500

    Empty success stories page

---
+++ res_pages/learn/LearnSuccess.js
@@ -0,0 +1,17 @@
+
+
+import * as React from "react";
+import * as Markdown$Ocamlorg from "../../components/Markdown.js";
+
+function $$default(param) {
+  return React.createElement(React.Fragment, undefined, React.createElement(Markdown$Ocamlorg.H1.make, {
+                  children: "Success Stories"
+                }));
+}
+
+export {
+  $$default ,
+  $$default as default,
+  
+}
+/* react Not a pure module */
---
+++ res_pages/learn/LearnSuccess.res
@@ -0,0 +1,9 @@
+// module LINK = Markdown.LINK
+module H1 = Markdown.H1
+
+let s = React.string
+
+let default = () =>
+  <>
+  <H1>{s(`Success Stories`)}</H1>
+  </>
