ac2a6b896c3f5af56e63df1281c20831ccb4f684
Kanishka Azimi <kazimi@solvuu.com>
2021-01-09 00:57:36 -0500

    Most text content copied, link text expanded for accessibility

---
+++ res_pages/docs/DocsLogos.js
@@ -0,0 +1,17 @@
+
+
+import * as React from "react";
+
+function $$default(param) {
+  return React.createElement(React.Fragment, undefined, React.createElement("h1", undefined, "OCaml logo"), React.createElement("p", undefined, "Web sites related to the OCaml language or that use it are encouraged to \n      incorporate the OCaml logo somewhere in their page to indicate their support \n      for OCaml development. The logo should be linked to the OCaml website, ocaml.org. \n      The ", "logo is available in several formats", ". Also, the ", "logo is available in several colors", ". Two versions are displayed below. To include one of them on your web page,\n      just copy and paste the associated HTML code and tweak it (e.g. adapt the ", "width", " or change the part after ", "logo/", " to match the path of the file you want to use in the ", "logo repository", "). The ", "OCaml logo is released under a liberal license", "."), React.createElement("p", undefined, "Here is one example:"), React.createElement("pre", undefined), React.createElement("p", undefined, "Here is another example:"), React.createElement("pre", undefined), React.createElement("h2", undefined, "Stickers"), React.createElement("img", {
+                  alt: "OCaml logo for sticker",
+                  src: "/static/OCaml_Sticker.svg"
+                }), React.createElement("p", undefined, "The following ", "OCaml sticker SVG file", "is suitable to make stickers, as seen in this", "tweet announcing stickers made", ". Similar to the logo, the ", "OCaml sticker file is released under a liberal license", ", so do no hesitate to print your own stickers to promote OCaml!"), React.createElement("p", undefined, "Before printing some stickers, it would be great if you tweeted it with \n      tags ", "#OCaml stickers", ", similar to these ", "OCaml stickers tweets", ", so that other interested people in your area can get in touch with you!"));
+}
+
+export {
+  $$default ,
+  $$default as default,
+  
+}
+/* react Not a pure module */
---
+++ res_pages/docs/DocsLogos.res
@@ -0,0 +1,52 @@
+// module LINK = Markdown.LINK
+
+let s = React.string
+
+let default = () =>
+  <>
+  <h1>{s(`OCaml logo`)}</h1>
+    <p>
+      {s(`Web sites related to the OCaml language or that use it are encouraged to 
+      incorporate the OCaml logo somewhere in their page to indicate their support 
+      for OCaml development. The logo should be linked to the OCaml website, ocaml.org. 
+      The `)}
+      {s(`logo is available in several formats`)}
+      {s(`. Also, the `)}
+      {s(`logo is available in several colors`)}
+      {s(`. Two versions are displayed below. To include one of them on your web page,
+      just copy and paste the associated HTML code and tweak it (e.g. adapt the `)}
+      {s(`width`)}
+      {s(` or change the part after `)}
+      {s(`logo/`)}
+      {s(` to match the path of the file you want to use in the `)}
+      {s(`logo repository`)}
+      {s(`). The `)}
+      {s(`OCaml logo is released under a liberal license`)}
+      {s(`.`)}
+    </p>
+    <p>{s(`Here is one example:`)}</p>
+    <pre>
+    </pre>
+    <p>{s(`Here is another example:`)}</p>
+    <pre>
+    </pre>
+    <h2>{s(`Stickers`)}</h2>
+    <img src="/static/OCaml_Sticker.svg" alt="OCaml logo for sticker"/>
+    <p>
+      {s(`The following `)}
+      {s(`OCaml sticker SVG file`)}
+      {s(`is suitable to make stickers, as seen in this`)}
+      {s(`tweet announcing stickers made`)}
+      {s(`. Similar to the logo, the `)}
+      {s(`OCaml sticker file is released under a liberal license`)}
+      {s(`, so do no hesitate to print your own stickers to promote OCaml!`)}
+    </p>
+    <p>
+      {s(`Before printing some stickers, it would be great if you tweeted it with 
+      tags `)}
+      {s(`#OCaml stickers`)}
+      {s(`, similar to these `)}
+      {s(`OCaml stickers tweets`)}
+      {s(`, so that other interested people in your area can get in touch with you!`)}
+    </p>
+  </>
