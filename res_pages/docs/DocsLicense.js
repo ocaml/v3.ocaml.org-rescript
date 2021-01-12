fca1e337ed4f93ae17ba0cd0a8ab7304aaa5949c
Kanishka Azimi <kazimi@solvuu.com>
2021-01-12 12:22:18 -0500

    Start copying license content

---
+++ res_pages/docs/DocsLicense.js
@@ -0,0 +1,14 @@
+
+
+import * as React from "react";
+
+function $$default(param) {
+  return React.createElement(React.Fragment, undefined, React.createElement("h1", undefined, "OCaml\'s License"), React.createElement("p", undefined, "In the following, “the OCaml Core System” refers to all files marked “Copyright INRIA” in this distribution."), React.createElement("p", undefined, "The OCaml Core System is distributed under the terms of the ", "GNU Lesser General Public License version 2.1", " (included below). "), React.createElement("p", undefined));
+}
+
+export {
+  $$default ,
+  $$default as default,
+  
+}
+/* react Not a pure module */
---
+++ res_pages/docs/DocsLicense.res
@@ -0,0 +1,19 @@
+// module LINK = Markdown.LINK
+
+let s = React.string
+
+let default = () =>
+  <>
+  <h1>{s(`OCaml's License`)}</h1>
+  <p>
+    {s(`In the following, “the OCaml Core System” refers to all files marked “Copyright INRIA” in this distribution.`)}
+  </p>
+  <p>
+    {s(`The OCaml Core System is distributed under the terms of the `)}
+    {s(`GNU Lesser General Public License version 2.1`)} 
+    {s(` (included below). `)}
+  </p>
+  <p>
+    
+  </p>
+  </>
