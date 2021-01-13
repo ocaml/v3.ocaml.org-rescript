7c2d3a373db5ac6bfa26ef5c2065cfc2f1fe5338
Kanishka Azimi <kazimi@solvuu.com>
2021-01-13 18:30:56 -0500

    Start page

---
+++ res_pages/community/planet/CommunityPlanetSyndication.js
@@ -0,0 +1,14 @@
+
+
+import * as React from "react";
+
+function $$default(param) {
+  return React.createElement(React.Fragment, undefined, React.createElement("h1", undefined, "OCaml Planet Syndication"));
+}
+
+export {
+  $$default ,
+  $$default as default,
+  
+}
+/* react Not a pure module */
---
+++ res_pages/community/planet/CommunityPlanetSyndication.res
@@ -0,0 +1,8 @@
+// module LINK = Markdown.LINK
+
+let s = React.string
+
+let default = () =>
+  <>
+  <h1>{s(`OCaml Planet Syndication`)}</h1>
+  </>
