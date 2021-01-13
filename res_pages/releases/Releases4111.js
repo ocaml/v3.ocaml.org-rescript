cd6b61d2165c03d3f64dd5f97aee34294506147f
Kanishka Azimi <kazimi@solvuu.com>
2021-01-13 16:36:46 -0500

    Start page

---
+++ res_pages/releases/Releases4111.js
@@ -0,0 +1,14 @@
+
+
+import * as React from "react";
+
+function $$default(param) {
+  return React.createElement(React.Fragment, undefined, React.createElement("h1", undefined, "OCaml 4.11.1"));
+}
+
+export {
+  $$default ,
+  $$default as default,
+  
+}
+/* react Not a pure module */
---
+++ res_pages/releases/Releases4111.res
@@ -0,0 +1,8 @@
+// module LINK = Markdown.LINK
+
+let s = React.string
+
+let default = () =>
+  <>
+  <h1>{s(`OCaml 4.11.1`)}</h1>
+  </>
