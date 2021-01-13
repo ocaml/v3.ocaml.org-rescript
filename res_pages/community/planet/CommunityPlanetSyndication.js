3ca4921448066d611ddfcd38b5ed746015e689d1
Kanishka Azimi <kazimi@solvuu.com>
2021-01-13 18:44:30 -0500

    Some content copied

--- res_pages/community/planet/CommunityPlanetSyndication.js
+++ res_pages/community/planet/CommunityPlanetSyndication.js
@@ -1,9 +1,13 @@
 
 
 import * as React from "react";
+import * as Markdown$Ocamlorg from "../../../components/Markdown.js";
 
 function $$default(param) {
-  return React.createElement(React.Fragment, undefined, React.createElement("h1", undefined, "OCaml Planet Syndication"));
+  return React.createElement(React.Fragment, undefined, React.createElement("h1", undefined, "OCaml Planet Syndication"), React.createElement("h2", undefined, "Guidelines"), React.createElement("p", undefined, "Two types of feeds are aggregated by the ", React.createElement(Markdown$Ocamlorg.LINK.make, {
+                      href: "/community/planet",
+                      children: "OCaml Planet"
+                    }), ": personal and institutional."), React.createElement("p", undefined, "Personal feeds are for individuals working with OCaml. Writing about OCaml \n        in every entry is not mandatory. On the contrary, this is an opportunity to broaden \n        the discussion. However, entries must respect the terms of use and the philosophy of \n        ocaml.org. Posts should avoid focusing on overtly commercial topics. If you write a \n        personal blog that also has many posts on other topics, we will be thankful if you \n        provide us with an already filtered feed (e.g., tagging posts and using a tag subfeed)."), React.createElement("h2", undefined, "How to syndicate your feed"), React.createElement("h2", undefined, "How to read planet from your RSS reader"));
 }
 
 export {
--- res_pages/community/planet/CommunityPlanetSyndication.res
+++ res_pages/community/planet/CommunityPlanetSyndication.res
@@ -1,8 +1,24 @@
-// module LINK = Markdown.LINK
+module LINK = Markdown.LINK
 
 let s = React.string
 
 let default = () =>
   <>
   <h1>{s(`OCaml Planet Syndication`)}</h1>
+    <h2>{s(`Guidelines`)}</h2>
+      <p>
+        {s(`Two types of feeds are aggregated by the `)}
+        <LINK href="/community/planet">{s(`OCaml Planet`)}</LINK>
+        {s(`: personal and institutional.`)}
+      </p>
+      <p>
+        {s(`Personal feeds are for individuals working with OCaml. Writing about OCaml 
+        in every entry is not mandatory. On the contrary, this is an opportunity to broaden 
+        the discussion. However, entries must respect the terms of use and the philosophy of 
+        ocaml.org. Posts should avoid focusing on overtly commercial topics. If you write a 
+        personal blog that also has many posts on other topics, we will be thankful if you 
+        provide us with an already filtered feed (e.g., tagging posts and using a tag subfeed).`)}
+      </p>
+    <h2>{s(`How to syndicate your feed`)}</h2>
+    <h2>{s(`How to read planet from your RSS reader`)}</h2>
   </>
