0bf86886c09cbc4dfa3fc70330d61de47592af9b
Kanishka Azimi <kazimi@solvuu.com>
2021-01-12 21:56:09 -0500

    Half of content copied

--- res_pages/caml-light/CamlLightIndex.js
+++ res_pages/caml-light/CamlLightIndex.js
@@ -3,7 +3,7 @@
 import * as React from "react";
 
 function $$default(param) {
-  return React.createElement(React.Fragment, undefined, React.createElement("h1", undefined, "Caml Light"), React.createElement("p", undefined));
+  return React.createElement(React.Fragment, undefined, React.createElement("h1", undefined, "Caml Light"), React.createElement("h2", undefined, "Overview"), React.createElement("p", undefined, "Caml Light is a lightweight, portable implementation of the core Caml language that was \n    developed in the early 1990\'s, as a precursor to OCaml. It used to run on most Unix machines, \n    as well as PC under Microsoft Windows. The implementation is obsolete, no longer actively \n    maintained, and will be removed eventually. We recommend switching immediately to its \n    successor, OCaml."), React.createElement("p", undefined, "Caml Light is implemented as a bytecode compiler, and fully bootstrapped. The runtime \n    system and bytecode interpreter is written in standard C, hence Caml Light is easy to port \n    to almost any 32 or 64 bit platform. The whole system is quite small: about 100K for the \n    runtime system, and another 100K of bytecode for the compiler. Two megabytes of memory is \n    enough to recompile the whole system."), React.createElement("p", undefined, "Debugging is possible by tracing function calls in the same way as in OCaml. In the \n    example therein, one should write \n    trace \"fib\";;\n     instead of \n    #trace fib;;\n     and \n    untrace \"fib\";; \n    instead of \n    #untrace fib;;\n    . There also exists a debugger, as a user contribution."), React.createElement("h2", undefined, "Availability"), React.createElement("p", undefined));
 }
 
 export {
--- res_pages/caml-light/CamlLightIndex.res
+++ res_pages/caml-light/CamlLightIndex.res
@@ -3,7 +3,35 @@ let s = React.string
 let default = () =>
   <>
   <h1>{s(`Caml Light`)}</h1>
+  <h2>{s(`Overview`)}</h2>
   <p>
-    
+    {s(`Caml Light is a lightweight, portable implementation of the core Caml language that was 
+    developed in the early 1990's, as a precursor to OCaml. It used to run on most Unix machines, 
+    as well as PC under Microsoft Windows. The implementation is obsolete, no longer actively 
+    maintained, and will be removed eventually. We recommend switching immediately to its 
+    successor, OCaml.`)}
+  </p>
+  <p>
+    {s(`Caml Light is implemented as a bytecode compiler, and fully bootstrapped. The runtime 
+    system and bytecode interpreter is written in standard C, hence Caml Light is easy to port 
+    to almost any 32 or 64 bit platform. The whole system is quite small: about 100K for the 
+    runtime system, and another 100K of bytecode for the compiler. Two megabytes of memory is 
+    enough to recompile the whole system.`)}
+  </p>
+  <p>
+    {s(`Debugging is possible by tracing function calls in the same way as in OCaml. In the 
+    example therein, one should write 
+    trace "fib";;
+     instead of 
+    #trace fib;;
+     and 
+    untrace "fib";; 
+    instead of 
+    #untrace fib;;
+    . There also exists a debugger, as a user contribution.`)}
+  </p>
+  <h2>{s(`Availability`)}</h2>
+  <p>
+
   </p>
   </>
