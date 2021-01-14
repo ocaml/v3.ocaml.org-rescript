35b7db170c6ec48d2fbad1c960c91a1345a2c6fb
Kanishka Azimi <kazimi@solvuu.com>
2021-01-14 17:47:47 -0500

    Content added

--- res_pages/design/DesignIndustry.js
+++ res_pages/design/DesignIndustry.js
@@ -3,7 +3,7 @@
 import * as React from "react";
 
 function $$default(param) {
-  return React.createElement(React.Fragment, undefined, React.createElement("h1", undefined, "Industrial Users of OCaml"));
+  return React.createElement(React.Fragment, undefined, React.createElement("h1", undefined, "Industrial Users of OCaml"), React.createElement("p", undefined, "Ocaml is a popular choice for companies who make use of its features in key\n      aspects of their technologies. Some companies that use OCaml code are listed below:"));
 }
 
 export {
